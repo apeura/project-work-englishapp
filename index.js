00a5a881301538f71d386738d11691a86e66e39f
apeura <anni.peura@tuni.fi>
2024-01-08 14:39:17 +0200

    Initial project push, set-up skeleton

---
+++ index.js
@@ -0,0 +1,43 @@
+// npm install express mysql cors dotenv
+// connect to mysql
+
+const express = require("express");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const port = 8080;
+
+const pool = mysql.createPool({
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  connectionLimit: 10,
+});
+
+app.get("/api/locations", (req, res) => {
+  pool.query("SELECT * FROM locations", (error, results) => {
+    if (error) {
+      console.error("Error fetching data:", error);
+      res.status(500).json({ error: "Internal Server Error!" });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+server = app
+  .listen(port, () => {
+    console.log(`SERVER: listening on port ${port}.`);
+    console.log(process.env); // ADD THIS!!
+  })
+  .on("error", (err) => {
+    console.error("SERVER: Error starting server: ", err);
+    process.exit(1);
+  });
+// docker ps -a + docker stop (container id)
+// docker-compose up -d -> (docker-compose exec app sh -> ls -la -> exit) -> docker-compose down
+// docker-compose logs
