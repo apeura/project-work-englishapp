0c9574fd6129b0f21ad90200606f15356d0ef861
apeura <anni.peura@tuni.fi>
2024-01-13 12:54:38 +0200

    Admin.jsx: Update useState submitted to table. Add useState: selectedTable, selectedRow, action. Update handler for adding a new word pair. Add starting point for updating word pair, selecting row from table. Add functions for placeholders/selected words.
    (Return): Restructure adminPanel:
        Add buttons to select action.
        Add editPanel, addPanel, delPanel.
        Update editPanel to include input boxes, display selected table.

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,25 +1,99 @@
 import { useState } from "react";
-import "../App.css";/* 
-import { addNewWordPair } from "../../../backend/util.js"; */
-/* import { PostRequest } from "../PostRequest";
- */function Admin() {
-  const [selectedTable, setSelectedTable] = useState();
-  const [submitted, setSubmitted] = useState(false); // submitted, for score display
+import Alert from "../Alert";
+import "../App.css";
+import AdminTable from "../AdminTable";
+
+function Admin() {
+  const [selectedTable, setSelectedTable] = useState(null);
+  const [selectedRow, setSelectedRow] = useState(null);
+  const [table, setTable] = useState(false); //table choice
+  const [action, setAction] = useState(false); //action choise del add edit
   const [engWord, setEngWord] = useState("");
   const [fiWord, setFiWord] = useState("");
 
   const handleChoice = (table) => {
     // open admin panel
-    setSubmitted(true);
+    setTable(true);
+    // set table
     setSelectedTable(table);
   };
 
-  const handleAddNewWordPair = () => {
-    console.log("Adding new word pair:", engWord, fiWord);/* 
-    addNewWordPair(selectedTable, engWord, fiWord); */
+  const handleActionChoice = (action) => {
+    console.log("action ", action);
+    setAction(action);
+  };
+  const handleUpdateWordPair = (async) => {
+    console.log("handleUpdateWordPair in use");
+    
+    let formattedEngWord = engWord.trim().toLowerCase();
+    let formattedFiWord = fiWord.trim().toLowerCase();
 
+    
+  };
+
+  const handleAddNewWordPair = async () => {
+    console.log("handleAddNewPair in use");
+
+    let formattedEngWord = engWord.trim().toLowerCase();
+    let formattedFiWord = fiWord.trim().toLowerCase();
+
+    try {
+      const response = await fetch(
+        // use set table
+        `http://localhost:8080/api/${selectedTable}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // use set words
+          body: JSON.stringify({
+            eng_word: formattedEngWord,
+            fi_word: formattedFiWord,
+          }),
+        }
+      );
+
+      if (response.ok) {
+        const result = await response.json();
+        console.log("Adding new word pair:", engWord, fiWord);
+        console.log(result);
+      } else {
+        console.error("Error adding new word pair:", response.status);
+      }
+    } catch (error) {
+      console.error("Error adding new word pair:", error);
+    }
     // close admin panel
-    //setSubmitted(false);
+    setTable(false);
+  };
+
+  const handleRowClick = (rowData) => {
+    setSelectedRow(rowData);
+    //console.log("admin row data ", rowData);
+  };
+
+  // edit
+  const getIdOrPlaceholder = () => {
+    if (selectedRow !== null) {
+      return selectedRow.id;
+    } else {
+      return "ID";
+    }
+  };
+  const getEngOrPlaceholder = () => {
+    if (selectedRow !== null) {
+      return selectedRow.engWord;
+    } else {
+      return "English Word";
+    }
+  };
+  const getFiOrPlaceholder = () => {
+    if (selectedRow !== null) {
+      return selectedRow.fiWord;
+    } else {
+      return "Finnish Word";
+    }
   };
 
   return (
@@ -27,12 +101,9 @@ import { addNewWordPair } from "../../../backend/util.js"; */
       <div></div>
       <h1>Admin Page</h1>
       <div className="card">
-        {/*         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
         <p>
-          Here you can Add new wordpairs for learning purposes.<br></br> Add to
-          table:
+          Here you can add, delete or update wordpairs for learning purposes.
+          <br></br> Choose which table you'll be handling.
         </p>
         <button onClick={() => handleChoice("Animals")}>Animals</button>
         <button onClick={() => handleChoice("Colors")}>Colors</button>
@@ -40,31 +111,84 @@ import { addNewWordPair } from "../../../backend/util.js"; */
         <button onClick={() => handleChoice("Jobs")}>Jobs</button>
         <button onClick={() => handleChoice("Foods")}>Foods</button>
       </div>
-      <div className={`adminPanel ${submitted ? "" : "hidden"}`}>
-        <h2>Adding to table: {selectedTable}</h2>
-        <div>
-          <input
-            type="text"
-            id="eng_word"
-            placeholder="english word"
-            value={engWord}
-            onChange={(e) => setEngWord(e.target.value)}
-          />
-          <input
-            type="text"
-            id="fi_word"
-            placeholder="finnish word"
-            value={fiWord}
-            onChange={(e) => setFiWord(e.target.value)}
-          />
+
+      <div className={`adminPanel ${table ? "" : "hidden"}`}>
+        {" "}
+        <h2>Chosen table: {selectedTable}</h2>
+        <p className="grey">What would you like to do?</p>
+        <button onClick={() => handleActionChoice("Add")}>Add</button>
+        <button onClick={() => handleActionChoice("Edit")}>Edit</button>
+        <button onClick={() => handleActionChoice("Delete")}>Delete</button>
+        <div className={`addPanel ${action === "Add" ? "" : "hidden"}`}>
+          <div>
+            <input
+              type="text"
+              placeholder="english word"
+              value={engWord}
+              onChange={(e) => setEngWord(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="finnish word"
+              value={fiWord}
+              onChange={(e) => setFiWord(e.target.value)}
+            />
+          </div>
+
+          <button
+            id="adminButton"
+            className="admin"
+            onClick={handleAddNewWordPair}
+            disabled={
+              !(engWord.trim().length >= 2 && fiWord.trim().length >= 2)
+            }
+          >
+            Add
+          </button>
+        </div>{" "}
+        <div className={`editPanel ${action === "Edit" ? "" : "hidden"}`}>
+          <div className="flex">
+            <div>
+              <h3>Which ID would you like to edit?</h3>
+              <input type="text" placeholder={getIdOrPlaceholder()} disabled />
+              <input
+                type="text"
+                value={engWord}
+                placeholder={getEngOrPlaceholder()}
+                onChange={(e) => setEngWord(e.target.value)}
+              />
+              <input
+                type="text"
+                value={fiWord}
+                placeholder={getFiOrPlaceholder()}
+                onChange={(e) => setFiWord(e.target.value)}
+              />
+            </div>
+            <button
+              id="adminButton"
+              className="admin"
+              onClick={handleUpdateWordPair}
+              disabled={
+                !(engWord.trim().length >= 2 && fiWord.trim().length >= 2)
+              }
+            >
+              Update Word Pair
+            </button>
+          </div>
+          <div className="flex">
+            <AdminTable
+              selectedTable={selectedTable}
+              onRowClick={handleRowClick}
+            />
+          </div>
         </div>
-        <button className="admin" onClick={handleAddNewWordPair}>
-          Add
-        </button>
+        <div className={`delPanel ${action === "Delete" ? "" : "hidden"}`}>
+          <p>DEL PANEL</p>
+        </div>
       </div>
+      {/*       <Alert /> */}
       <p className="grey">
-{/*         <PostRequest />
- */}        word thing here?word thing here? word thing here? word thing here? word{" "}
+        word thing here?word thing here? word thing here? word thing here? word{" "}
       </p>
     </>
   );
