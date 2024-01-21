b44a7f87aae2bb667a3cf5b5556f6d64a6227e8c
apeura <anni.peura@tuni.fi>
2024-01-21 18:59:30 +0200

    Troubleshooting CORS, add app.get CORS headers.

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,15 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
+app.get((req, res, next) => {
+  /**
+   * Set CORS headers for get routes.
+   */
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 /**
  * GET request endpoint for fetching a list of tables from the database.
  * @param {Object} req - Express request object.
